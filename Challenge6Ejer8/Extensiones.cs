8fcf6bcdbb030507c4122d6ac31025e793a73b2d
NicolasFrancolino <87394098+NicolasFrancolino@users.noreply.github.com>
2022-05-10 13:48:51 -0300

    Agregar archivos de proyecto.

---
+++ Challenge6Ejer8/Extensiones.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer8
+{
+    static class Extensiones
+    {
+        public static string PrimerMitad(this string palabra)
+        {
+            string aux = palabra.Substring(0, palabra.Length / 2);
+            return aux;
+        }
+
+        public static string SegundaMitad(this string palabra)
+        {
+            string aux = palabra.Substring(palabra.Length / 2, palabra.Length - (palabra.Length / 2));
+            return aux;
+        }
+    }
+}
---
+++ Challenge6Ejer8/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge6Ejer8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nico = "Nicolas";
+            Console.WriteLine(nico);
+            Console.WriteLine(nico.PrimerMitad());
+            Console.WriteLine(nico.SegundaMitad());
+
+            Console.ReadKey();
+        }
+    }
+}
